92f573b5a20f4277b71bccd282e9f4fa1965b09d
Thomas Smyth <thomas.smyth3141@gmail.com>
2020-03-26 20:51:04 +0000

    Linux Support

--- squad-server/log-parser/rules/connection-steam-id.js
+++ squad-server/log-parser/rules/connection-steam-id.js
@@ -1,5 +1,5 @@
 export default {
-  regex: /\[([0-9.:-]+)]\[([ 0-9]*)]LogEasyAntiCheatServer: \[([0-9.:-]+)]\[([A-z]*)]\[EAC Server] \[Info]\[RegisterClient] Client: ([A-Z0-9]+) PlayerGUID: ([0-9]{17}) PlayerIP: ([0-9]{17}) OwnerGUID: ([0-9]{17}) PlayerName: (.+)/,
+  regex: /\[([0-9.:-]+)]\[([ 0-9]*)]LogEasyAntiCheatServer: \[([0-9.:-]+)]\[([A-z]*)]\[EAC Server] \[Info]\[RegisterClient] Client: ([A-z0-9]+) PlayerGUID: ([0-9]{17}) PlayerIP: ([0-9]{17}) OwnerGUID: ([0-9]{17}) PlayerName: (.+)/,
   action: (args, logParser) => {
     logParser.connectionHandler.newSteamID(args, logParser);
   }
--- squad-server/log-parser/rules/player-wound.js
+++ squad-server/log-parser/rules/player-wound.js
@@ -1,5 +1,5 @@
 export default {
-  regex: /\[([0-9.:-]+)]\[([ 0-9]*)]LogSquadTrace: \[DedicatedServer]ASQSoldier::Wound\(\): Player:(.+) KillingDamage=([0-9.]+) from ([A-z_0-9]+) caused by ([A-z_0-9]+)/,
+  regex: /\[([0-9.:-]+)]\[([ 0-9]*)]LogSquadTrace: \[DedicatedServer](?:ASQSoldier::)*Wound\(\): Player:(.+) KillingDamage=([0-9.]+) from ([A-z_0-9]+) caused by ([A-z_0-9]+)/,
   action: (args, logParser) => {
     logParser.injuryHandler.newWound(args, logParser);
   }
