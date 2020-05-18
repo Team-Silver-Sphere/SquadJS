3115a4804459ddbc4bafe80a6ad5b514b5b4698d
Thomas Smyth <thomas.smyth3141@gmail.com>
2020-05-18 19:53:34 +0100

    Add option to disable SCBL links in discord TK log

--- plugins/discord-teamkill/index.js
+++ plugins/discord-teamkill/index.js
@@ -22,6 +22,7 @@ export default async function(
 
   options = {
     color: 16761867,
+    disableSCBL: false,
     ...options
   };
 
@@ -30,40 +31,43 @@ export default async function(
   server.on(LOG_PARSER_TEAMKILL, info => {
     if (!info.attacker) return;
 
+    const fields = [
+      {
+        name: "Attacker's Name",
+        value: info.attacker.name,
+        inline: true
+      },
+      {
+        name: "Attacker's SteamID",
+        value: `[${info.attacker.steamID}](https://steamcommunity.com/profiles/${info.attacker.steamID})`,
+        inline: true
+      },
+      {
+        name: 'Weapon',
+        value: info.weapon
+      },
+      {
+        name: "Victim's Name",
+        value: info.victim.name,
+        inline: true
+      },
+      {
+        name: "Victim's SteamID",
+        value: `[${info.victim.steamID}](https://steamcommunity.com/profiles/${info.victim.steamID})`,
+        inline: true
+      }
+    ];
+
+    if(!options.disableSCBL) fields.push({
+      name: 'Squad Community Ban List',
+      value: `[Attacker's Bans](https://squad-community-ban-list.com/search/${info.attacker.steamID})\n[Victims's Bans](https://squad-community-ban-list.com/search/${info.victim.steamID})`
+    });
+
     channel.send({
       embed: {
         title: `${info.attacker.steamID === info.victim.steamID ? 'Suicide' : 'Teamkill'}: ${info.attacker.name}`,
         color: options.color,
-        fields: [
-          {
-            name: "Attacker's Name",
-            value: info.attacker.name,
-            inline: true
-          },
-          {
-            name: "Attacker's SteamID",
-            value: `[${info.attacker.steamID}](https://steamcommunity.com/profiles/${info.attacker.steamID})`,
-            inline: true
-          },
-          {
-            name: 'Weapon',
-            value: info.weapon
-          },
-          {
-            name: "Victim's Name",
-            value: info.victim.name,
-            inline: true
-          },
-          {
-            name: "Victim's SteamID",
-            value: `[${info.victim.steamID}](https://steamcommunity.com/profiles/${info.victim.steamID})`,
-            inline: true
-          },
-          {
-            name: 'Squad Community Ban List',
-            value: `[Attacker's Bans](https://squad-community-ban-list.com/search/${info.attacker.steamID})\n[Victims's Bans](https://squad-community-ban-list.com/search/${info.victim.steamID})`
-          }
-        ],
+        fields: fields,
         timestamp: info.time.toISOString(),
         footer: {
           text: COPYRIGHT_MESSAGE
