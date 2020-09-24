1c6ceb88872bc4ec68355d7401e9ebd2c7638096
SeanWalsh95 <SeanWalsh95@users.noreply.github.com>
2020-09-24 12:51:32 -0400

    Better updates (#1)
    * ignore config.json
    
    this is to prevent accidently uploading sensitive information that may be in config.json as well as the ability to update SquadJS via git pull without overriding config.json as it is no longer tracked
    
    * replace config.json with example-config.json
    
    replaces the initial function of config.json with example-config.json and adds code to create config.json from example-config.json if it doesn’t already exist on initial run

--- factory/read-config.js
+++ factory/read-config.js
@@ -6,7 +6,15 @@ const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
 export default function (configPath = './config.json') {
   const fullConfigPath = path.resolve(__dirname, '../', configPath);
-  if (!fs.existsSync(fullConfigPath)) throw new Error('Config file does not exist.');
+  const fullExamplePath = path.resolve(__dirname, '../', './example-config.json');
+
+  if (!fs.existsSync(fullConfigPath)) {
+    fs.copyFile(fullExamplePath, fullConfigPath, (err) => {
+      if (err) throw err;
+      throw new Error('Config generated from example, Configure config.json and run again.');
+    });
+  }
+
   const unparsedConfig = fs.readFileSync(fullConfigPath, 'utf8');
 
   let parsedConfig;
