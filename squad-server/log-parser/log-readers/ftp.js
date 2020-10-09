aa4004ae9fe9d70e33934dadc6e008e2df790801
Patrick <privat@skyraptor.eu>
2020-10-09 18:29:44 +0200

    Add ftpHost option
    Allow to use ftpHost if set, otherwise fall back to host.

--- squad-server/log-parser/log-readers/ftp.js
+++ squad-server/log-parser/log-readers/ftp.js
@@ -6,12 +6,12 @@ export default class TailLogReader {
   constructor(queueLine, options = {}) {
     if (typeof queueLine !== 'function')
       throw new Error('queueLine argument must be specified and be a function.');
-    if (!options.host) throw new Error('host argument must be specified.');
+    if (!options.ftpHost && !options.host) throw new Error('ftpHost or host argument must be specified.');
     if (!options.ftpUser) throw new Error('user argument must be specified.');
     if (!options.ftpPassword) throw new Error('password argument must be specified.');
 
     this.reader = new FTPTail({
-      host: options.host,
+      host: options.ftpHost ?? options.host,
       port: options.ftpPort || 21,
       user: options.ftpUser,
       password: options.ftpPassword,
