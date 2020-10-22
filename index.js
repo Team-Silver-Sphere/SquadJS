cb92d168ed3cb4edbed27a2b584166f4e5505f17
Thomas Smyth <thomas.smyth3141@gmail.com>
2020-10-22 23:30:43 +0100

    Add outdated version warning

--- index.js
+++ index.js
@@ -1,18 +1,18 @@
 import SquadServer from 'squad-server';
 import printLogo from 'squad-server/logo';
 
-printLogo();
+async function main() {
+  await printLogo();
 
-const config = process.env.config;
-const configPath = process.argv[2];
+  const config = process.env.config;
+  const configPath = process.argv[2];
+  if (config && configPath) throw new Error('Cannot accept both a config and config path.');
 
-if (config && configPath) throw new Error('Cannot accept both a config and config path.');
+  const server = config
+    ? await SquadServer.buildFromConfigString(config)
+    : await SquadServer.buildFromConfigFile(configPath || './config.json');
 
-let server;
-if (config) {
-  server = SquadServer.buildFromConfigString(config);
-} else {
-  server = SquadServer.buildFromConfigFile(configPath || './config.json');
+  await server.watch();
 }
 
-server.then((server) => server.watch());
+main();
