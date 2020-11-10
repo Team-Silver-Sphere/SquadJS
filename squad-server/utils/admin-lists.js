c7b2763cc7a73c32f21af17b2ae2e7f67f81c34f
SeanWalsh95 <SeanWalsh95@users.noreply.github.com>
2020-11-10 11:31:24 -0500

    AdminList suport for local files

--- squad-server/utils/admin-lists.js
+++ squad-server/utils/admin-lists.js
@@ -1,5 +1,11 @@
+import fs from 'fs';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
 import axios from 'axios';
 import Logger from 'core/logger';
+
+const __dirname = fileURLToPath(import.meta.url);
 
 export default async function fetchAdminLists(adminLists) {
   Logger.verbose('SquadServer', 2, `Fetching Admin Lists...`);
@@ -9,43 +15,65 @@ export default async function fetchAdminLists(adminLists) {
 
   for (let idx = 0; idx < adminLists.length; idx++) {
     const list = adminLists[idx];
+    try {
+      let data = '';
 
-    let data = '';
-
-    if (list.type === 'remote') {
-      try {
-        const resp = await axios({
-          method: 'GET',
-          url: `${list.loc}`
-        });
-        data = resp.data;
-      } catch (error) {
-        Logger.verbose('SquadServer', 1, `Error fetching remote admin list ${list.loc}`, error);
+      switch (list.type) {
+        case 'remote': {
+          const resp = await axios({
+            method: 'GET',
+            url: `${list.loc}`
+          });
+          data = resp.data;
+          break;
+        }
+        case 'local': {
+          const listPath = path.resolve(__dirname, '../../../', list.loc);
+          if (!fs.existsSync(listPath)) throw new Error(`Could not find Admin List at ${listPath}`);
+          data = fs.readFileSync(listPath, 'utf8');
+          break;
+        }
+        default:
+          throw new Error(`Unsupported AdminList type:${list.type}`);
       }
-    }
 
-    const groupRgx = /(?<=Group=)(.*?):(.*)(?=\n)/g;
-    const adminRgx = /(?<=Admin=)(\d+):(\S+)(?=\s)/g;
+      const groupRgx = /(?<=Group=)(.*?):(.*)(?=(?:\r\n|\r|\n))/g;
+      const adminRgx = /(?<=Admin=)(\d+):(\S+)(?=\s)/g;
 
-    /* eslint-disable no-unused-vars */
-    for (const [match, groupID, groupPerms] of data.matchAll(groupRgx)) {
-      groups[`${idx}-${groupID}`] = groupPerms.split(',');
-    }
-    for (const [match, steamID, groupID] of data.matchAll(adminRgx)) {
-      const perms = {};
-      for (const perm of groups[`${idx}-${groupID}`]) perms[perm] = true;
+      /* eslint-disable no-unused-vars */
+      for (const [match, groupID, groupPerms] of data.matchAll(groupRgx)) {
+        groups[`${idx}-${groupID}`] = groupPerms.split(',');
+      }
+      for (const [match, steamID, groupID] of data.matchAll(adminRgx)) {
+        const perms = {};
+        for (const perm of groups[`${idx}-${groupID}`]) perms[perm] = true;
 
-      if (steamID in admins) admins[steamID] = Object.assign(admins[steamID], perms);
-      else admins[steamID] = Object.assign(perms);
-
+        if (steamID in admins) {
+          admins[steamID] = Object.assign(admins[steamID], perms);
+          Logger.verbose(
+            'SquadServer',
+            3,
+            `Merged duplicate Admin ${steamID} to ${Object.keys(admins[steamID])}`
+          );
+        } else {
+          admins[steamID] = Object.assign(perms);
+          Logger.verbose(
+            'SquadServer',
+            3,
+            `Added Admin ${steamID} with ${Object.keys(admins[steamID])}`
+          );
+        }
+      }
+      /* eslint-enable no-unused-vars */
+    } catch (error) {
       Logger.verbose(
         'SquadServer',
-        3,
-        `Added Admin ${steamID} with ${Object.keys(admins[steamID])}`
+        1,
+        `Error fetching ${list.type} admin list: ${list.loc}`,
+        error
       );
     }
-    /* eslint-enable no-unused-vars */
   }
-  Logger.verbose('SquadServer', 2, `${Object.keys(admins).length} admins loaded...`);
+  Logger.verbose('SquadServer', 1, `${Object.keys(admins).length} admins loaded...`);
   return admins;
 }
