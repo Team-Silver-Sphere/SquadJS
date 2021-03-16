3339f78df6b6488bf0447ca2926afa40cc0bc1ab
SeanWalsh95 <SeanWalsh95@users.noreply.github.com>
2021-03-16 15:52:24 -0400

    Add Helicopter and Tank count to layer info
    simplified to reduce commit spam

--- squad-server/layers/layer.js
+++ squad-server/layers/layer.js
@@ -15,33 +15,33 @@ export default class Layer {
       name: data.lighting,
       classname: data.lightingLevel
     };
-    this.teams = [
-      {
-        faction: data.team1.faction,
-        name: data.team1.teamSetupName,
-        tickets: data.team1.tickets,
-        commander: data.team1.commander,
-        vehicles: (data.team1.vehicles || []).map((vehicle) => ({
+    this.teams = [];
+    this.tanks = [];
+    this.helicopters = [];
+    for (const t of ['team1', 'team2']) {
+      this.teams.push({
+        faction: data[t].faction,
+        name: data[t].teamSetupName,
+        tickets: data[t].tickets,
+        commander: data[t].commander,
+        vehicles: (data[t].vehicles || []).map((vehicle) => ({
           name: vehicle.type,
           classname: vehicle.rawType,
           count: vehicle.count,
           spawnDelay: vehicle.delay,
           respawnDelay: vehicle.respawnTime
         }))
-      },
-      {
-        faction: data.team2.faction,
-        name: data.team2.teamSetupName,
-        tickets: data.team2.tickets,
-        commander: data.team2.commander,
-        vehicles: (data.team2.vehicles || []).map((vehicle) => ({
-          name: vehicle.type,
-          classname: vehicle.rawType,
-          count: vehicle.count,
-          spawnDelay: vehicle.delay,
-          respawnDelay: vehicle.respawnTime
-        }))
-      }
-    ];
+      });
+      this.tanks.push(
+        data[t].vehicles.filter((v) => {
+          return v.icon.match(/tank/);
+        }).length
+      );
+      this.helicopters.push(
+        data[t].vehicles.filter((v) => {
+          return v.icon.match(/helo/);
+        }).length
+      );
+    }
   }
 }
