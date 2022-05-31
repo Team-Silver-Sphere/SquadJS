098bf7d3685f8df4c29114c7fab0fae16da62a05
bravo2zero <izinchenko@gmail.com>
2022-05-31 22:17:14 +0200

    update new game log regex

--- squad-server/log-parser/new-game.js
+++ squad-server/log-parser/new-game.js
@@ -1,6 +1,6 @@
 export default {
   regex:
-    /^\[([0-9.:-]+)]\[([ 0-9]*)]LogWorld: Bringing World \/([A-z\/]+)\/Maps\/([A-z]+)\/(?:Gameplay_Layers\/)?([A-z0-9_]+)/, // eslint-disable-line
+    /^\[([0-9.:-]+)]\[([ 0-9]*)]LogWorld: Bringing World \/([A-z]+)\/(?:Maps\/)?([A-z0-9-]+)\/(?:[A-z0-9-/]+\/)?([A-z0-9-]+)(?:\.[A-z0-9-]+)/, // eslint-disable-line
   onMatch: (args, logParser) => {
     const data = {
       ...logParser.eventStore.WON,
