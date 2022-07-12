61a20ef97258f9926765b9a745f2c36dd9633148
SeanWalsh95 <SeanWalsh95@users.noreply.github.com>
2022-07-12 15:06:03 -0400

    allow for victim to be notified on TK

--- squad-server/plugins/auto-tk-warn.js
+++ squad-server/plugins/auto-tk-warn.js
@@ -11,10 +11,20 @@ export default class AutoTKWarn extends BasePlugin {
 
   static get optionsSpecification() {
     return {
-      message: {
+      attacker_message: {
         required: false,
-        description: 'The message to warn players with.',
+        description: 'The message to warn attacking players with.',
         default: 'Please apologise for ALL TKs in ALL chat!'
+      },
+      warn_victim: {
+        required: false,
+        description: 'Should we nottify the the victim, telling them that they were teamkilled via a warning message',
+        default: false
+      },
+      victim_message: {
+        required: false,
+        description: 'The message that will be sent to the victim',
+        default: 'You were killed by your own team.'
       }
     };
   }
@@ -34,8 +44,14 @@ export default class AutoTKWarn extends BasePlugin {
   }
 
   async onTeamkill(info) {
-    if (!info.attacker) return;
+    if (info.attacker) {
+      this.server.rcon.warn(info.attacker.steamID, this.options.attacker_message);
+    };
+    
+    if (this.options.warn_victe && info.victim) {
+      this.server.rcon.warn(info.victim.steamID, this.options.victim_message);
+    };
 
-    await this.server.rcon.warn(info.attacker.steamID, this.options.message);
+    
   }
 }
