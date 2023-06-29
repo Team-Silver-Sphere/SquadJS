ede41d0386771db402a5e569eaec94a5d8a0e3b8
Magic <magicoflolis@tuta.io>
2023-06-28 17:54:18 -0700

    Improved log-parser/index.js
    Signed-off-by: Magic <magicoflolis@tuta.io>

--- core/log-parser/index.js
+++ core/log-parser/index.js
@@ -8,6 +8,8 @@ import Logger from '../logger.js';
 import TailLogReader from './log-readers/tail.js';
 import FTPLogReader from './log-readers/ftp.js';
 
+const addedLines = [];
+
 export default class LogParser extends EventEmitter {
   constructor(filename = 'filename.log', options = {}) {
     super();
@@ -46,24 +48,28 @@ export default class LogParser extends EventEmitter {
   async processLine(line) {
     Logger.verbose('LogParser', 4, `Matching on line: ${line}`);
 
-    for (const rule of this.getRules()) {
+    let i = this.getRules().length;
+    while (i--) {
+      const rule = this.getRules()[i];
       const match = line.match(rule.regex);
       if (!match) continue;
-
+      addedLines.push({ rule, match });
+    }
+    this.linesPerMinute += 1;
+    this.onLine(addedLines);
+    addedLines.length = 0;
+  }
+	
+	onLine(addedLine) {
+    for(const ad of addedLine) {
+      const { rule, match } = ad;
       Logger.verbose('LogParser', 3, `Matched on line: ${match[0]}`);
-
       match[1] = moment.utc(match[1], 'YYYY.MM.DD-hh.mm.ss:SSS').toDate();
       match[2] = parseInt(match[2]);
-
       rule.onMatch(match, this);
-
-      this.matchingLinesPerMinute++;
-      this.matchingLatency += Date.now() - match[1];
-
-      break;
-    }
-
-    this.linesPerMinute++;
+      this.matchingLinesPerMinute += 1;
+      this.matchingLatency +=  Number(Date.now()) - match[1];
+    };
   }
 
   // manage cleanup disconnected players, session data.
@@ -100,7 +106,7 @@ export default class LogParser extends EventEmitter {
       } lines per minute | Matching lines per minute: ${
         this.matchingLinesPerMinute
       } matching lines per minute | Average matching latency: ${
-        this.matchingLatency / this.matchingLinesPerMinute
+        Number.isNaN(this.matchingLatency / this.matchingLinesPerMinute) ? 0 : this.matchingLatency / this.matchingLinesPerMinute
       }ms`
     );
     this.linesPerMinute = 0;
