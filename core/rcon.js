1f6e61288a1ed4c81aa6fdc5b99ebedccefb79e7
Fantino Davide <fantinodavide02@gmail.com>
2023-12-14 03:08:14 +0100

    fix: typos as suggested in https://github.com/Team-Silver-Sphere/SquadJS/pull/318#issuecomment-1854740474

--- core/rcon.js
+++ core/rcon.js
@@ -228,7 +228,7 @@ export default class Rcon extends EventEmitter {
       this.emit(`response${this.responseString.id - 2}`, this.responseString.body);
       this.responseString.body = '';
     } else if (!packet.body.includes('')) {
-      this.responseString.body = this.responseString.body += packet.body;
+      this.responseString.body = this.responseString.body + packet.body;
       this.responseString.id = packet.id;
     } else this.badPacket();
   }
@@ -241,7 +241,7 @@ export default class Rcon extends EventEmitter {
       }`
     );
     this.stream = Buffer.alloc(0);
-    this.responseString = '';
+    this.responseString.body = '';
     return null;
   }
   onClose() {
