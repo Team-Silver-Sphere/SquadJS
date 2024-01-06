f155ed879de67cef9b6794634d4194c01ee9e368
werewolfboy13 <werewolfboy13@afocommunity.com>
2024-01-05 18:39:36 -0600

    Removing broken CDN method.

--- squad-server/utils/print-logo.js
+++ squad-server/utils/print-logo.js
@@ -21,7 +21,7 @@ function versionOutOfDate(current, latest) {
 
 export default async function () {
   const { data } = await axios.get(
-    `https://cdn.jsdelivr.net/gh/Team-Silver-Sphere/SquadJS@master/package.json`
+    `https://raw.githubusercontent.com/Team-Silver-Sphere/SquadJS/master/package.json`
   );
   const outdated = versionOutOfDate(SQUADJS_VERSION, data.version);
 
