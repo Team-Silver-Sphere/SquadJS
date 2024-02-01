5f76b43bbed9b8f888ee6f0b24e7c9c79566259c
IgnisAlienus <andrewhuntkvfd@gmail.com>
2024-02-01 10:49:54 -0600

    Feat: Support for GC's Player Controllers

---
+++ squad-server/log-parser/player-connected-gc.js
@@ -0,0 +1,26 @@
+export default {
+    regex:
+      /^\[([0-9.:-]+)]\[([ 0-9]*)]LogSquad: PostLogin: NewPlayer: BP_GC_PlayerController_C .+PersistentLevel\.([^\s]+) \(IP: ([\d.]+) \| Online IDs: EOS: ([0-9a-f]{32}) steam: (\d+)\)/,
+    onMatch: (args, logParser) => {
+      const data = {
+        raw: args[0],
+        time: args[1],
+        chainID: +args[2],
+        playercontroller: args[3],
+        ip: args[4],
+        eosID: args[5],
+        steamID: args[6]
+      };
+  
+      const joinRequestData = logParser.eventStore.joinRequests[+args[2]];
+      data.connection = joinRequestData.connection;
+      data.playerSuffix = joinRequestData.suffix;
+  
+      if (!logParser.eventStore.players[data.steamID])
+        logParser.eventStore.players[data.steamID] = {};
+      logParser.eventStore.players[data.steamID].controller = data.playercontroller;
+  
+      logParser.emit('PLAYER_CONNECTED', data);
+    }
+  };
+  
