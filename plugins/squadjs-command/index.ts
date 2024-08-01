343e0c37a3c6ec40a8660d56808ffd0387b10432
Thomas Smyth <thomas.smyth3141@gmail.com>
2024-08-01 23:23:19 +0100

    Add dependency to demo

--- plugins/squadjs-command/index.ts
+++ plugins/squadjs-command/index.ts
@@ -1,14 +1,18 @@
+import winston from 'winston';
 import { Plugin } from '../../src/plugin-system';
+
+const logger: winston.Logger = winston.createLogger({
+  transports: [new winston.transports.Console()]
+});
 
 export default class SquadJSCommand extends Plugin {
   async onChatMessage(data: any): Promise<void> {
-    console.log(data);
-
     // Check whether the message contained the SquadJS command.
     const command = data.message.match(/!squadjs/);
 
     // Send the response to the command.
     if (command) {
+      logger.info(`${data.player.name} called the !squadjs command.`);
       await this.server.rcon.warn(data.player.eosID, 'This server is running SquadJS!');
     }
   }
