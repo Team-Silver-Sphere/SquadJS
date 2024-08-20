9ebda081617ee38a51acd47a1733fd39e1c08b24
Mike Barton <dev@barton.gg>
2024-08-19 21:27:30 -0700

    Added FTP support to admin lists.

--- squad-server/utils/admin-lists.js
+++ squad-server/utils/admin-lists.js
@@ -1,6 +1,8 @@
 import fs from 'fs';
 import path from 'path';
 import { fileURLToPath } from 'url';
+import { Client as FTPClient } from 'basic-ftp'
+import WritableBuffer from "./writable-buffer.js"
 
 import axios from 'axios';
 import Logger from 'core/logger';
@@ -30,6 +32,40 @@ export default async function fetchAdminLists(adminLists) {
           if (!fs.existsSync(listPath)) throw new Error(`Could not find Admin List at ${listPath}`);
           data = fs.readFileSync(listPath, 'utf8');
           break;
+        }
+        case 'ftp': {
+          // ex uri:  ftp://username:password@host:21/some/file.txt
+          if (!list.source.startsWith('ftp://')) {
+            throw new Error(`Invalid FTP URI format of ${list.source}. The source must be a FTP URI starting with the protocol. Ex: ftp://username:password@host:21/some/file.txt`)
+          }
+          const [loginString, hostPathString] = list.source.substring('ftp://'.length).split("@")
+          const [user, password] = loginString.split(":").map(v => decodeURI(v))
+          const pathStartIndex = hostPathString.indexOf("/")
+          const remoteFilePath = pathStartIndex === -1 ? "/" : hostPathString.substring(pathStartIndex)
+          const [host, port = 21] = hostPathString.substring(0, pathStartIndex === -1 ? hostPathString.length : pathStartIndex).split(":")
+
+          console.log("ftp debug loginString", loginString)
+          console.log("ftp debug hostPathString", hostPathString)
+          console.log("ftp debug user", user)
+          console.log("ftp debug password", password)
+          console.log("ftp debug pathStartIndex", pathStartIndex)
+          console.log("ftp debug remoteFilePath", remoteFilePath)
+          console.log("ftp debug host", host)
+          console.log("ftp debug port", port)
+
+          const buffer = new WritableBuffer()
+          const ftpClient = new FTPClient()
+          await ftpClient.access({
+            host,
+            port,
+            user,
+            password
+          })
+          await ftpClient.downloadTo(buffer, remoteFilePath)
+          const bufData = buffer.toString("utf8")
+          console.log("ftp debug buffer", bufData)
+          data = bufData
+          break
         }
         default:
           throw new Error(`Unsupported AdminList type:${list.type}`);
