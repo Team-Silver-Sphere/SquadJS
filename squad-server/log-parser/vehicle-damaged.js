19bf8bb52d763e00b4d025288f07e8dfa9bdafd4
Misha Yudin <misha.yudin19@gmail.com>
2025-01-26 17:46:35 +0200

    Add VEHICLE_DAMAGED server event

---
+++ squad-server/log-parser/vehicle-damaged.js
@@ -0,0 +1,22 @@
+export default {
+  regex:
+    /^\[([0-9.:-]+)]\[([ 0-9]*)]LogSquadTrace: \[DedicatedServer](?:ASQVehicleSeat::)?TraceAndMessageClient\(\): ([A-z0-9_]+)_C_[0-9]+: ([0-9.]+) damage taken by causer ([A-z0-9_]+)_C_[0-9]+ instigator \(Online Ids: (.+)\) EOS: ([A-z0-9_]+) steam: ([0-9]+) health remaining (-?[0-9.]+)/,
+  onMatch: (args, logParser) => {
+    const data = {
+      raw: args[0],
+      time: args[1],
+      chainID: args[2],
+      vehicle: args[3],
+      damage: parseFloat(args[4]),
+      weapon: args[5],
+      attackerName: args[6],
+	  attackerEOSID: args[7],
+	  attackerSteamID: args[8],
+      healthRemaining: parseFloat(args[9])
+    };
+
+    logParser.eventStore.session[args[3]] = data;
+
+    logParser.emit('VEHICLE_DAMAGED', data);
+  }
+};
