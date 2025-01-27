331133b592a6b25e16e4f7d06143010f54658ad1
Misha Yudin <misha.yudin19@gmail.com>
2025-01-27 19:20:09 +0200

    Rewritten the logic to determine teamkill based on confidence calculated from player 'Side' and vehicle 'Sides'

--- squad-server/log-parser/vehicle-damaged.js
+++ squad-server/log-parser/vehicle-damaged.js
@@ -1,38 +1,42 @@
-import WeaponsLookup from './lookup/weapons-lookup.js';
 import VehiclesLookup from './lookup/vehicles-lookup.js';
+import FactionSides from './lookup/factions-lookup.js';
 
 export default {
   regex:
     /^\[([0-9.:-]+)]\[([ 0-9]*)]LogSquadTrace: \[DedicatedServer](?:ASQVehicleSeat::)?TraceAndMessageClient\(\): ([A-z0-9_]+)_C_[0-9]+: ([0-9.]+) damage taken by causer ([A-z0-9_]+)_C_[0-9]+ instigator \(Online Ids: (.+)\) EOS: ([A-z0-9_]+) steam: ([0-9]+) health remaining (-?[0-9.]+)/,
   onMatch: (args, logParser) => {
+	const data = {
+		raw: args[0],
+		time: args[1],
+		chainID: args[2],
+		vehicle: args[3],
+		damage: parseFloat(args[4]),
+		weapon: args[5],
+		attackerName: args[6],
+		attackerEOSID: args[7],
+		attackerSteamID: args[8],
+		healthRemaining: parseFloat(args[9]),
+		vehicleTeams: VehiclesLookup[args[3]],
+		teamkill: null
+	};
+	
 	const isTeamkill = (() => {
-		const attackerWeapon = WeaponsLookup[args[5]];
-		const targetVehicle = VehiclesLookup[args[3]];
-		const teamkillPresumption = 0;
-		if (attackerWeapon && targetVehicle) {
-			for (const targetTeam of targetVehicle) { // performed against targetVehicle, since most of them are 1-long arrays, unlike weapons
-				if (attackerWeapon.includes(targetTeam)) {teamkillPresumption = teamkillPresumption + 1;}
-			}
-			return teamkillPresumption === 0;
+		const vehicleTeamsCheck = data.vehicleTeams;
+		const player = logParser.eventStore.players[data.attackerEOSID];
+		if (!(player && player.teamName && vehicleTeamsCheck?.length)) return null;
+		const playerSide = FactionSides[player.teamName];
+		if (!playerSide) return null;
+		const vehicleSide = [];
+		for (const vehTeam of vehicleTeamsCheck) {
+			const vehSide = FactionSides[vehTeam];
+			if (vehSide && !vehicleSide.includes(vehSide)) vehicleSide.push(vehSide);
 		}
-		return false;
+		if (vehicleSide.includes(playerSide)) return 1.0 / vehicleSide.length > 0.5; // teamkill confidence
+		else return false; // not in team list
 	})();
-    const data = {
-      raw: args[0],
-      time: args[1],
-      chainID: args[2],
-      vehicle: args[3],
-      damage: parseFloat(args[4]),
-      weapon: args[5],
-      attackerName: args[6],
-	  attackerEOSID: args[7],
-	  attackerSteamID: args[8],
-      healthRemaining: parseFloat(args[9]),
-	  teamkill: isTeamkill
-    };
-
-    logParser.eventStore.session[args[3]] = data;
-
+	
+	data.teamkill = isTeamkill;
+    
     logParser.emit('VEHICLE_DAMAGED', data);
   }
 };
