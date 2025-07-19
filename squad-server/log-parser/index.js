5e2b74e966532512ce85fd2985e3adbaec61201a
Ethan Snow <bombitmanbomb@gmail.com>
2025-07-19 07:22:08 -0400

    Make parser imports OS agnostic

--- squad-server/log-parser/index.js
+++ squad-server/log-parser/index.js
@@ -2,35 +2,30 @@ import fs from 'fs';
 import path from 'path';
 
 import LogParser from 'core/log-parser';
-import Logger from 'core/logger';
-import { fileURLToPath } from 'url';
+import Logger from '../../core/logger.js';
+import { fileURLToPath, pathToFileURL } from 'url';
 
 const __dirname = path.dirname(fileURLToPath(import.meta.url));
-
 export default class SquadLogParser extends LogParser {
   constructor(options) {
     super('SquadGame.log', options);
     this._rules = [];
-  }
-
-  async watch() {
-    //? Wait for rules to be configured before hooking the log file
-    await this.setupRules();
-    return super.watch();
+    this.setupRules();
   }
 
   async setupRules() {
-    const files = await fs.promises.opendir(path.join(__dirname, './'));
+    const files = await fs.promises.opendir(path.resolve(path.join(__dirname, './')));
     for await (const file of files) {
       if (!file.isFile() || !file.name.endsWith('.js')) continue;
       Logger.verbose('SquadLogParser', 1, `Loading parser file ${file.name}...`);
-      const module = await import(path.join(__dirname, file.name));
+      const module = await import(pathToFileURL(path.join(__dirname, file.name)).href);
       if ('default' in module && 'regex' in module.default && 'onMatch' in module.default) {
         this._rules.push(module.default);
       }
     }
   }
 
+  // This should use a Loader in Vanilla SquadJS
   getRules() {
     return this._rules;
   }
