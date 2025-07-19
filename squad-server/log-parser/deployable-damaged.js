cb55ffad80c561f09bb1e441712370521458fdc7
Makavich <159694550+dycadmin@users.noreply.github.com>
2025-07-19 19:42:06 +0300

    Update deployable-damaged.js
    supports GE

--- squad-server/log-parser/deployable-damaged.js
+++ squad-server/log-parser/deployable-damaged.js
@@ -1,21 +1,24 @@
 export default {
-  regex:
-    /^\[([0-9.:-]+)]\[([ 0-9]*)]LogSquadTrace: \[DedicatedServer](?:ASQDeployable::)?TakeDamage\(\): ([A-z0-9_]+)_C_[0-9]+: ([0-9.]+) damage attempt by causer ([A-z0-9_]+)_C_[0-9]+ instigator (.+) with damage type ([A-z0-9_]+)_C health remaining ([0-9.]+)/,
+  regex: /^\[([0-9.:-]+)]\[([0-9]+)]LogSquadTrace: \[DedicatedServer\]TakeDamage\(\): ([A-Za-z0-9_]+)_C_[0-9]+: ([0-9.]+) damage (?:taken|attempt) by causer ([A-Za-z0-9_]+)_C_[0-9]+ instigator ([^()]+?)(?: \(Online IDs: EOS: ([a-f0-9]+) steam: (\d+)\))?(?: with damage type ([A-Za-z0-9_]+)_C)? health remaining ([0-9.]+)/,
+
   onMatch: (args, logParser) => {
     const data = {
       raw: args[0],
-      time: args[1],
+      time: new Date(),
       chainID: args[2],
       deployable: args[3],
       damage: parseFloat(args[4]),
       weapon: args[5],
-      playerSuffix: args[6],
-      damageType: args[7],
-      healthRemaining: args[8]
+      player: {
+        name: args[6]?.trim() || 'Unknown',
+        eosID: args[7] || null,
+        steamID: args[8] || null
+      },
+      damageType: args[9] || 'Unknown',
+      healthRemaining: parseFloat(args[10])
     };
 
     logParser.eventStore.session[args[3]] = data;
-
     logParser.emit('DEPLOYABLE_DAMAGED', data);
   }
 };
